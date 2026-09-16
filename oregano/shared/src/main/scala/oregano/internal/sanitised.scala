2d199f14d9195912fa6cd62c56516bac9c2df4c8
Charlie Shaw <charliecpshaw@gmail.com>
2026-09-16 17:14:51 +0100

    feat (sanitised): add sanitised

---
+++ oregano/shared/src/main/scala/oregano/internal/sanitised.scala
@@ -0,0 +1,9 @@
+package oregano.internal
+
+import scala.quoted.Expr
+
+object sanitised {
+  type SanitiseExpr[A] = Expr[Option[Sanitised[A]]]
+
+  case class Sanitised[+A](captures: A, any: Boolean)
+}
