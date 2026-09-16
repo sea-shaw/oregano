4a7177f656af8330d08b665ee913aaa87044987c
Charlie Shaw <charliecpshaw@gmail.com>
2026-09-16 17:13:06 +0100

    feat (hchain): add hchain

---
+++ oregano/shared/src/main/scala/oregano/internal/hchain.scala
@@ -0,0 +1,22 @@
+package oregano.internal
+
+/* Heterogeneous data structure with constant time concatenation. See cats Chain
+   for the non-heterogeneous version. */
+// TODO: cats copyright notice?
+object hchain {
+  sealed trait HChain
+
+  /* Empty HChain. Cannot appear inside an `HAppend`. */
+  type HEmpty = HEmpty.type
+  case object HEmpty extends HChain
+
+  /* Non-empty `HChain`. Used to enforce the invariant that `HAppend` cannot
+     contain `HEmpty`. */
+  sealed trait HNonEmpty extends HChain
+
+  /* Singleton `HChain`. Appears at the leaves of all non-empty `HChain`s. */
+  case class HSingleton[+A](value: A) extends HNonEmpty
+
+  /* Concatenation of two non-empty `HChain`s. `Append` name comes from cats. */
+  case class HAppend[+A <: HNonEmpty, +B <: HNonEmpty](left: A, right: B) extends HNonEmpty
+}
