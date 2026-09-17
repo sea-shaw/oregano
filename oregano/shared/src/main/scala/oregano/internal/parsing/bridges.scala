a36026a3c7f2913fb9d18617473ff5d3fa718251
Charlie Shaw <charliecpshaw@gmail.com>
2026-09-17 13:07:05 +0100

    feat (parsing): copy parser, bridges, and errors

---
+++ oregano/shared/src/main/scala/oregano/internal/parsing/bridges.scala
@@ -0,0 +1,124 @@
+package oregano.internal.parsing
+
+import cats.collections.Diet
+import cats.data.NonEmptyList
+import oregano.internal.ast.{AST, QuantifierType}
+import parsley.Parsley
+import parsley.Parsley.pure
+import parsley.bridges.ParserSingletonBridge
+import parsley.errors.combinator.*
+import parsley.templates.{PureParserBridge1, PureParserBridge2, PureParserBridge3}
+import scala.quoted.Quotes
+
+object bridges {
+
+  type ToRegex = (ast: AST, q: Quotes) ?=> ast.Regex[?]
+
+  object Dot extends ParserSingletonBridge[ToRegex] {
+    override protected def singleton: Parsley[ToRegex] = pure(ast.Dot())
+  }
+
+  object Lit extends PureParserBridge1[Int, ToRegex] {
+    override def apply(c: Int): ToRegex = ast.Lit(c)
+  }
+
+  object Class extends PureParserBridge1[Diet[Int], ToRegex] {
+    override def apply(cs: Diet[Int]): ToRegex = ast.Class(cs)
+  }
+
+  object LineStart extends ParserSingletonBridge[ToRegex] {
+    override protected def singleton: Parsley[ToRegex] = pure(ast.LineStart())
+  }
+
+  object LineEnd extends ParserSingletonBridge[ToRegex] {
+    override protected def singleton: Parsley[ToRegex] = pure(ast.LineEnd())
+  }
+
+  object NegativeLookahead extends PureParserBridge1[ToRegex, ToRegex] {
+    override def apply(inner: ToRegex): ToRegex = ast.NegativeLookahead(inner)
+  }
+
+  object NegativeLookbehind extends PureParserBridge1[ToRegex, ToRegex] {
+    override def apply(inner: ToRegex): ToRegex = ast.NegativeLookbehind(inner)
+  }
+
+  object Backreference extends PureParserBridge1[Int, ToRegex] {
+    override def apply(group: Int): ToRegex = ast.Backreference(group)
+  }
+
+  object Capture extends PureParserBridge1[ToRegex, ToRegex] {
+    override def apply(inner: ToRegex): ToRegex = ast.Capture(inner)
+  }
+
+  object NamedCapture extends PureParserBridge2[String, ToRegex, ToRegex] {
+    override def apply(name: String, inner: ToRegex): ToRegex = ast.NamedCapture(name, inner)
+  }
+
+  object PositiveLookahead extends PureParserBridge1[ToRegex, ToRegex] {
+    override def apply(inner: ToRegex): ToRegex = ast.PositiveLookahead(inner)
+  }
+
+  object PositiveLookbehind extends PureParserBridge1[ToRegex, ToRegex] {
+    override def apply(inner: ToRegex): ToRegex = ast.PositiveLookbehind(inner)
+  }
+
+  object Independent extends PureParserBridge1[ToRegex, ToRegex] {
+    override def apply(inner: ToRegex): ToRegex = ast.Independent(inner)
+  }
+
+  object Cat extends PureParserBridge1[NonEmptyList[ToRegex], ToRegex] {
+    override def apply(regexes: NonEmptyList[ToRegex]): ToRegex = {
+      val NonEmptyList(head, tail) = regexes
+      tail.foldLeft(head)(ast.Cat(_, _))
+    }
+  }
+
+  object Alt extends PureParserBridge2[ToRegex, ToRegex, ToRegex] {
+    override def apply(left: ToRegex, right: ToRegex): ToRegex = ast.Alt(left, right)
+  }
+
+  object Opt extends PureParserBridge2[ToRegex, QuantifierType, ToRegex] {
+    override def apply(inner: ToRegex, quantifierType: QuantifierType): ToRegex = ast.Opt(inner, quantifierType)
+  }
+
+  object Star extends PureParserBridge2[ToRegex, QuantifierType, ToRegex] {
+    override def apply(inner: ToRegex, quantifierType: QuantifierType): ToRegex =  ast.Star(inner, quantifierType)
+  }
+
+  object Plus extends PureParserBridge2[ToRegex, QuantifierType, ToRegex] {
+    override def apply(inner: ToRegex, quantifierType: QuantifierType): ToRegex =  ast.Plus(inner, quantifierType)
+  }
+
+  object NumericalQuantifier {
+    def apply(start: Parsley[Int], end: Parsley[Option[Option[Int]]]): Parsley[(ToRegex, QuantifierType) => ToRegex] =  (start <~> end).mapFilterMsg {
+      case (0, None | Some(Some(0))) => Right((toRegex, _) => ast.Zero(toRegex))
+      case (1, None | Some(Some(1))) => Right((toRegex, _) => toRegex)
+      case (n, None)                 => Right(ast.Exactly(_, n, _))
+      case (0, Some(None))           => Right(ast.Star(_, _))
+      case (n, Some(None))           => Right(ast.AtLeast(_, n, _))
+      case (0, Some(Some(1)))        => Right(ast.Opt(_, _))
+      case (0, Some(Some(m)))        => Right(ast.AtMost(_, m, _))
+      case (n, Some(Some(m)))        => if n == m then Right(ast.Exactly(_, n, _))
+                                        else if n < m then Right(ast.Between(_, n, m, _))
+                                        else Left(Seq("Upper bound cannot be less than lower bound"))
+    }
+  }
+
+  object WithFlags extends PureParserBridge3[List[Char], Option[NonEmptyList[Char]], Option[ToRegex], ToRegex] {
+    override def apply(on: List[Char], off: Option[NonEmptyList[Char]], mInner: Option[ToRegex]): ToRegex = {
+      val (onSet, offSet) = flags(on, off)
+      mInner match {
+        case None        => ast.Flags(onSet, offSet)
+        case Some(inner) => ast.NonCapture(onSet, offSet, inner)
+      }
+    }
+  }
+
+  private inline def ast(using ast: AST): ast.type = ast
+
+  private def flags(on: List[Char], off: Option[NonEmptyList[Char]]): (Set[Char], Set[Char]) = {
+    val onSet = on.toSet
+    val offSet = off.fold(Nil)(_.toList).toSet
+    (onSet -- offSet, offSet)
+  }
+}
---
+++ oregano/shared/src/main/scala/oregano/internal/parsing/errors.scala
@@ -0,0 +1,74 @@
+package oregano.internal.parsing
+
+import parsley.errors.{DefaultErrorBuilder, ErrorBuilder}
+import parsley.errors.tokenextractors.TillNextWhitespace
+
+object errors {
+  case class Pos(line: Int, offset: Int, width: Int)
+  case class PosError(msg: String, pos: Pos)
+  case class PosErrorInfoLines(lines: Seq[String], pos: Pos)
+
+  object PosErrorBuilder extends ErrorBuilder[PosError] with TillNextWhitespace {
+
+    override def build(pos: Position, source: Source, lines: ErrorInfoLines): PosError = {
+      PosError(lines.lines.mkString("\n"), lines.pos)
+    }
+
+    type Position = Unit
+
+    type Source = Unit
+
+    override def pos(line: Int, col: Int): Position = ()
+
+    override def source(sourceName: Option[String]): Source = ()
+
+    type ErrorInfoLines = PosErrorInfoLines
+
+    override def vanillaError(unexpected: UnexpectedLine, expected: ExpectedLine, reasons: Messages, line: LineInfo): ErrorInfoLines = {
+      PosErrorInfoLines(Seq.concat(unexpected, expected, reasons), line)
+    }
+
+    override def specializedError(msgs: Messages, line: LineInfo): ErrorInfoLines = PosErrorInfoLines(msgs, line)
+
+    type ExpectedItems = Option[String]
+    type Messages = Seq[Message]
+
+    override def combineExpectedItems(alts: Set[Item]): ExpectedItems = DefaultErrorBuilder.disjunct(alts)
+
+    override def combineMessages(alts: Seq[Message]): Messages = DefaultErrorBuilder.combineMessages(alts)
+
+    type UnexpectedLine = Option[String]
+    type ExpectedLine = Option[String]
+    type Message = String
+    type LineInfo = Pos
+
+    override def unexpected(item: Option[Item]): UnexpectedLine = DefaultErrorBuilder.unexpected(item)
+
+    override def expected(alts: ExpectedItems): ExpectedLine = DefaultErrorBuilder.expected(alts)
+
+    override def reason(reason: String): Message = DefaultErrorBuilder.reason(reason)
+
+    override def message(msg: String): Message = DefaultErrorBuilder.message(msg)
+
+    override def lineInfo(line: String, linesBefore: Seq[String], linesAfter: Seq[String], lineNum: Int, errorPointsAt: Int, errorWidth: Int): LineInfo = {
+      Pos(lineNum - 1, errorPointsAt, errorWidth)
+    }
+
+    override val numLinesBefore: Int = 0
+
+    override val numLinesAfter: Int = 0
+
+    type Item = String
+    type Raw = String
+    type Named = String
+    type EndOfInput = String
+
+    override def raw(item: String): Raw = DefaultErrorBuilder.raw(item)
+
+    override def named(item: String): Named = DefaultErrorBuilder.named(item)
+
+    override val endOfInput: EndOfInput = DefaultErrorBuilder.EndOfInput
+
+    override val trimToParserDemand: Boolean = true
+  }
+}
